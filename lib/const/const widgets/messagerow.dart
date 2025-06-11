0ffab1c7fb899e7285488d6c1e2d77a567d0e697
shahd523 <shahdehab798@gmail.com>
2025-06-12 02:06:55 +0300

    updated const widgets

--- lib/const/const widgets/messagerow.dart
+++ lib/const/const widgets/messagerow.dart
@@ -16,18 +16,18 @@ class MessageRow extends StatelessWidget{
         //Get.offAll(chatscreen);},
         child: Row(mainAxisSize: MainAxisSize.max,
           children: [
-          ClipRRect(borderRadius: BorderRadius.circular(50),
-            child: Image.asset(
-              "assets/images/Image AR.png", fit: BoxFit.cover, height: 50, width: 50,),
-          ),
-          SizedBox(width: 4),
-          Column(children: [
-            Text("اسم المستخدم",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 16),),
-            SizedBox(height: 5),
-            Text("نص الرساله",style: TextStyle(fontWeight: FontWeight.w200,fontSize: 12),)
-          ],)
+            ClipRRect(borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                "assets/images/Image AR.png", fit: BoxFit.cover, height: 50, width: 50,),
+            ),
+            SizedBox(width: 4),
+            Column(children: [
+              Text("اسم المستخدم",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 16),),
+              SizedBox(height: 5),
+              Text("نص الرساله",style: TextStyle(fontWeight: FontWeight.w200,fontSize: 12),)
+            ],)
 
-        ],),
+          ],),
       ),
     );
   }
