9f76a8d99077084e64f64424cbd449bf580cda31
shahd523 <shahdehab798@gmail.com>
2025-06-13 21:58:36 +0300

    updated const widgets

---
+++ lib/const/const widgets/messagecard.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class messagecard extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:
+      [
+        ClipRRect(borderRadius: BorderRadius.circular(50),
+          child: Image.asset(
+            "assets/images/Image AR.png", fit: BoxFit.cover, height: 50, width: 50,),
+        ),
+        SizedBox(height: 2,),
+        Text("اسم المستخدم",style: TextStyle(fontSize: 10,fontWeight: FontWeight.w500,color: Colors.black),),
+
+      ],
+
+
+
+
+    );
+  }
+
+}
+/*Column(
+          children: [
+            ClipRRect(borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                "assets/images/Image AR.png", fit: BoxFit.cover, height: 50, width: 50,),
+            ),
+            SizedBox(height: 2,),
+            Text("اسم المستخدم",style: TextStyle(fontSize: 10,fontWeight: FontWeight.w500,color: Colors.black),),
+
+          ],
+        ),
+        SizedBox(width:5),
+        Column(
+          children: [
+            ClipRRect(borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                "assets/images/Image AR.png", fit: BoxFit.cover, height: 50, width: 50,),
+            ),
+            SizedBox(height: 2,),
+            Text("اسم المستخدم",style: TextStyle(fontSize: 10,fontWeight: FontWeight.w500,color: Colors.black),),
+
+          ],
+        ),
+        SizedBox(width:5),
+        Column(
+          children: [
+            ClipRRect(borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                "assets/images/Image AR.png", fit: BoxFit.cover, height: 50, width: 50,),
+            ),
+            SizedBox(height: 2,),
+            Text("اسم المستخدم",style: TextStyle(fontSize: 10,fontWeight: FontWeight.w500,color: Colors.black),),
+
+          ],
+        ),
+        SizedBox(width:5),
+        Column(
+          children: [
+            ClipRRect(borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                "assets/images/Image AR.png", fit: BoxFit.cover, height: 50, width: 50,),
+            ),
+            SizedBox(height: 2,),
+            Text("اسم المستخدم",style: TextStyle(fontSize: 10,fontWeight: FontWeight.w500,color: Colors.black),),
+
+          ],
+        ),
+        SizedBox(width:5),
+        Column(
+          children: [
+            ClipRRect(borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                "assets/images/Image AR.png", fit: BoxFit.cover, height: 50, width: 50,),
+            ),
+            SizedBox(height: 2,),
+            Text("اسم المستخدم",style: TextStyle(fontSize: 10,fontWeight: FontWeight.w500,color: Colors.black),),
+
+          ],
+        ),*/
