8fbb361b5a01bc8f66ca8738040be8ae737dbc74
omarashour222 <omarabdoulrahman@gmail.com>
2025-07-23 19:36:45 +0300

    favourite

--- lib/const/const widgets/apartmint_card.dart
+++ lib/const/const widgets/apartmint_card.dart
@@ -6,7 +6,8 @@ import 'package:saken_mobile/New%20Screens/housing%20profile%20screen/views/hous
 import 'package:saken_mobile/const/const.dart';
 
 class ApartmentCard extends StatelessWidget {
-  const ApartmentCard({super.key});
+  final bool fav;
+  const ApartmentCard({super.key, this.fav = false});
 
   @override
   Widget build(BuildContext context) {
@@ -24,20 +25,43 @@ class ApartmentCard extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              Container(
-                width: double.infinity,
-                height: 107,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  image: const DecorationImage(
-                    image: AssetImage('assets/images/Room.png'),
-                    fit: BoxFit.cover,
+              Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 107,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/Room.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                ),
+                  Positioned(
+                    top: 5,
+                    right: 5,
+                    child: Container(
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.8),
+                          shape: BoxShape.circle,
+                        ),
+                        child: fav
+                            ? Icon(
+                                CupertinoIcons.heart_fill,
+                                size: 18,
+                                color: Colors.red,
+                              )
+                            : Icon(
+                                CupertinoIcons.heart,
+                                size: 18,
+                                color: Colors.grey,
+                              )),
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 8,
-              ),
+              const SizedBox(height: 8),
               const Row(
                 children: [
                   Text('شقة للايجار'),
@@ -45,50 +69,30 @@ class ApartmentCard extends StatelessWidget {
                   Icon(Icons.more_horiz_outlined),
                 ],
               ),
-              const SizedBox(
-                height: 5,
-              ),
+              const SizedBox(height: 5),
               const Row(
                 children: [
-                  Icon(
-                    Icons.location_on_outlined,
-                    size: 20,
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
+                  Icon(Icons.location_on_outlined, size: 20),
+                  SizedBox(width: 5),
                   Text(
                     'حي شرق, اسكندرية',
-                    style: TextStyle(
-                      fontSize: 12,
-                    ),
+                    style: TextStyle(fontSize: 12),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 5,
-              ),
+              const SizedBox(height: 5),
               const Row(
                 children: [
                   Icon(Icons.bed_outlined),
-                  SizedBox(
-                    width: 5,
-                  ),
+                  SizedBox(width: 5),
                   Text('2 غرفة'),
                   Spacer(),
-                  Icon(
-                    Icons.bathroom_outlined,
-                    size: 20,
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
+                  Icon(Icons.bathroom_outlined, size: 20),
+                  SizedBox(width: 5),
                   Text('1 حمام'),
                 ],
               ),
-              const SizedBox(
-                height: 5,
-              ),
+              const SizedBox(height: 5),
               const Row(
                 children: [
                   Text(
@@ -100,9 +104,7 @@ class ApartmentCard extends StatelessWidget {
                   ),
                   Spacer(),
                   Text('4.5'),
-                  SizedBox(
-                    width: 3,
-                  ),
+                  SizedBox(width: 3),
                   Icon(
                     CupertinoIcons.star_fill,
                     color: Colors.amber,
--- lib/screens/home_page/TabBarWidgets/sakanwidget.dart
+++ lib/screens/home_page/TabBarWidgets/sakanwidget.dart
@@ -1,24 +1,25 @@
-import 'package:flutter/material.dart';
-import 'package:saken_mobile/const/Models/apartmentmodel.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:saken_mobile/const/Routes.dart';
 import 'package:saken_mobile/const/const%20widgets/apartmint_card.dart';
-import 'package:saken_mobile/const/routes.dart';
 
 class sakanwidget extends StatelessWidget {
-  bool fav;
-  sakanwidget({super.key, this.fav = true});
+  final bool fav;
+  const sakanwidget({super.key, this.fav = true});
+
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1 / 1.4,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 8),
+        crossAxisCount: 2,
+        childAspectRatio: 1 / 1.4,
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+      ),
       itemBuilder: (context, index) => GestureDetector(
         onTap: () {
           Navigator.pushNamed(context, Routes.sakanDetails);
         },
-        child: ApartmentCard(),
+        child: ApartmentCard(fav: fav), 
       ),
       itemCount: 8,
     );
